f00a280835704f8185ef0cd0e917f6849c67c5bc
ElSombrero2 <nirilala15@gmail.com>
2026-01-06 12:20:09 +0100

    initial commit

---
+++ src/main/java/org/madacraft/template/Template.java
@@ -0,0 +1,10 @@
+package org.madacraft.template;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Template extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("Hello World");
+    }
+}
